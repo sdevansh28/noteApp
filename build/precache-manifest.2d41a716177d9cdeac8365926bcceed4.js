86e0d6207ae9ad78965ac933b97a5c342282312c
Devansh Sharma <sdevansh28@gmail.com>
2022-03-13 17:11:37 +0530

    added icon

---
+++ build/precache-manifest.2d41a716177d9cdeac8365926bcceed4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae052e8b91ef351158acde2aa0558fb",
+    "url": "/noteapp/index.html"
+  },
+  {
+    "revision": "c4bf385d1127cd642e78",
+    "url": "/noteapp/static/js/2.104b377d.chunk.js"
+  },
+  {
+    "revision": "b80a1400bcf242765c1a",
+    "url": "/noteapp/static/js/main.3886bfae.chunk.js"
+  },
+  {
+    "revision": "1a0c290a542f3cdd1cf3",
+    "url": "/noteapp/static/js/runtime-main.d94c0356.js"
+  }
+]);
